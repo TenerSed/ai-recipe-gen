88898ebce0c58d6ee752eb3b5db771e267f33b62
TenerSed <74030082+TenerSed@users.noreply.github.com>
2025-08-08 13:32:05 -0400

    Update start_training.py

--- start_training.py
+++ start_training.py
@@ -1,13 +1,15 @@
 import subprocess
+import torch
+
 
 def start_training():
 # YOLOv8 training command with AMP (automatic mixed precision) enabled
 		command = (
 				'start "" cmd /c "yolo task=detect mode=train '
-				'model=yolov8l.pt '
+				'model=yolov8m.pt '
 				'data=data/fridge_data/data.yaml '
 				'epochs=30 imgsz=640 batch=32 name=fridge_yolo cache=True '
-				'device=0 amp=True workers=12 > train_log.txt 2>&1"'
+				'device=0 amp=True workers=16 > train_log.txt 2>&1"'
 		)
 	# Launch in new CMD window
 		subprocess.run(command, shell=True)
@@ -19,11 +21,13 @@ def resume_training():
         'model=runs/detect/fridge_yolo/weights/last.pt '
         'data=data/fridge_data/data.yaml '
         'imgsz=640 cache=True '
-        'resume=True device=0 amp=True workers=12 > resume_log.txt 2>&1"'
+        'resume=True device=0 amp=True workers=16 > resume_log.txt 2>&1"'
     )
     subprocess.run(command, shell=True)
 
 
 if __name__ == "__main__":
-    start_training()
+		torch.cuda.empty_cache()
+		start_training()
+  
     # resume_training()
