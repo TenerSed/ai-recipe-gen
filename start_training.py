6881cfb174e2de970711cb78df911e69bfe1751d
TenerSed <74030082+TenerSed@users.noreply.github.com>
2025-08-08 12:39:11 -0400

    Update start_training.py

--- start_training.py
+++ start_training.py
@@ -4,9 +4,9 @@ def start_training():
 # YOLOv8 training command with AMP (automatic mixed precision) enabled
 		command = (
 				'start "" cmd /c "yolo task=detect mode=train '
-				'model=yolov8n.pt '
+				'model=yolov8m.pt '
 				'data=data/fridge_data/data.yaml '
-				'epochs=30 imgsz=960 batch=32 name=fridge_yolo '
+				'epochs=30 imgsz=640 batch=32 name=fridge_yolo '
 				'device=0 amp=True workers=8 > train_log.txt 2>&1"'
 		)
 	# Launch in new CMD window
@@ -16,14 +16,14 @@ def resume_training():
     # YOLOv8 resume training command from last.pt checkpoint
     command = (
         'start "" cmd /c "yolo task=detect mode=train '
-        'model=runs/detect/fridge_yolo2/weights/last.pt '
+        'model=runs/detect/fridge_yolo/weights/last.pt '
         'data=data/fridge_data/data.yaml '
         'imgsz=640 '
-        'resume=True device=0 amp=True workers=6 > resume_log.txt 2>&1"'
+        'resume=True device=0 amp=True workers=8 > resume_log.txt 2>&1"'
     )
     subprocess.run(command, shell=True)
 
 
 if __name__ == "__main__":
-    # start_training()
-    resume_training()
+    start_training()
+    # resume_training()
