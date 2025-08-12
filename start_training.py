73985865980debd56cccb29b729c8fe88be68d32
TenerSed <sid.songirkar@icloud.com>
2025-08-12 18:18:38 -0400

    finish training

--- start_training.py
+++ start_training.py
@@ -8,8 +8,8 @@ def start_training():
 				'start "" cmd /c "yolo task=detect mode=train '
 				'model=yolov8m.pt '
 				'data=data/fridge_data/data.yaml '
-				'epochs=30 imgsz=640 batch=32 name=fridge_yolo cache=True '
-				'device=0 amp=True workers=16 > train_log.txt 2>&1"'
+				'epochs=30 imgsz=512 batch=32 name=fridge_yolo cache=disk '
+                'device=0 amp=True workers=12 > train_log.txt 2>&1"'
 		)
 	# Launch in new CMD window
 		subprocess.run(command, shell=True)
@@ -18,16 +18,16 @@ def resume_training():
     # YOLOv8 resume training command from last.pt checkpoint
     command = (
         'start "" cmd /c "yolo task=detect mode=train '
-        'model=runs/detect/fridge_yolo/weights/last.pt '
+        'model=runs/detect/fridge_yolo5/weights/last.pt '
         'data=data/fridge_data/data.yaml '
-        'imgsz=640 cache=True '
-        'resume=True device=0 amp=True workers=16 > resume_log.txt 2>&1"'
+        'epochs=60 imgsz=512 cache=disk '
+        'resume=True device=0 amp=True workers=12 > resume_log.txt 2>&1"'
     )
     subprocess.run(command, shell=True)
 
 
 if __name__ == "__main__":
-		torch.cuda.empty_cache()
-		start_training()
+	#torch.cuda.empty_cache()
+    #start_training()
   
-    # resume_training()
+    resume_training()
